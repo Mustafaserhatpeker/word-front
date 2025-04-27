7a1e6036882b03365424b377ad759d82aff834a9
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-04-27 16:52:36 +0300

    Refactor code structure for improved readability and maintainability

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-
+import { ImageBackground } from "react-native";
 import { Box } from "@/components/ui/box";
-
 import { Text } from "@/components/ui/text";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "expo-router";
@@ -9,15 +8,21 @@ import { useRouter } from "expo-router";
 export default function Home() {
   const router = useRouter();
   return (
-    <Box className="flex-1 h-[100vh]">
-      <Text>selam</Text>
-      <Button
-        onPress={() => {
-          router.push("/auth/login");
-        }}
-      >
-        <Text>Click me</Text>
-      </Button>
-    </Box>
+    <ImageBackground
+      source={require("../assets/images/bg.webp")}
+      style={{ flex: 1 }}
+      resizeMode="cover"
+    >
+      <Box className="flex-1">
+        <Text>selam</Text>
+        <Button
+          onPress={() => {
+            router.push("/auth/login");
+          }}
+        >
+          <Text>Click me</Text>
+        </Button>
+      </Box>
+    </ImageBackground>
   );
 }
